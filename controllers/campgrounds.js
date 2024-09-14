e1b1f97037aa1fa605786bc034ee935ba286e69e
Sahil Pahuja <19059570004@aryabhattacollege.ac.in>
2024-09-15 00:22:10 +0530

    made the website a little responsive and improved the overall design

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -7,11 +7,11 @@ const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds, page: 'index'});
+    res.render('campgrounds/index', { campgrounds});
 }
 
 module.exports.renderNewForm = (req, res) => {
-    res.render('campgrounds/new',{ page: 'new' });
+    res.render('campgrounds/new');
 }
 
 module.exports.createCampground = async (req, res, next) => {
@@ -40,7 +40,7 @@ module.exports.showCampground = async (req, res,) => {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
     }
-    res.render('campgrounds/show', { campground , page: 'show'});
+    res.render('campgrounds/show', { campground});
 }
 
 module.exports.renderEditForm = async (req, res) => {
@@ -50,7 +50,7 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
     }
-    res.render('campgrounds/edit', { campground , page: 'edit'});
+    res.render('campgrounds/edit', { campground});
 }
 
 module.exports.updateCampground = async (req, res) => {
---
+++ temp.js
@@ -0,0 +1,120 @@
+const mongoose = require("mongoose");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+require('dotenv').config();
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+mbxGeocoding({ accessToken: mapBoxToken });
+const axios = require("axios");
+const key = process.env.API_KEY;
+const mainAuth = process.env.OWNER_ID;
+const { cloudinary } = require("../cloudinary");
+const { reverseGeo } = require("../tools/index");
+// const mainAuth = '62040b04c7e98a10d8c2d8ac';
+
+//get the model 
+const Campground = require('../models/campground');
+const User = require('../models/user');
+
+//initial connection error
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl)
+    .then(() => {
+        console.log('CONNECTION MONGO OPEN!')
+    })
+    .catch((err) => {
+        console.log('OH NO MONGO ERROR')
+        console.log(err)
+    })
+
+//after initial connection error
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//get seeding data from nps api
+const processDatas = async () => {
+    try {
+        const config = {
+            params:
+            {
+                api_key: key
+            }
+        };
+        const res = await axios.get(`https://developer.nps.gov/api/v1/campgrounds?limit=300`, config);
+        return res;
+    } catch (e) {
+        console.log("Connection timeout")
+        console.log(e);
+    }
+}
+
+async function upload(images, camp) {
+    for (let i = 0; i < images.length; i++) {
+        try {
+            //store the result after upload and insert in camp images
+            const res = await cloudinary.uploader.upload_large(images[i], { folder: 'YelpCamp' });
+            camp.images.push({ url: res.secure_url, filename: res.original_filename });
+        } catch (e) {
+            if (i === 0) {
+                camp.success = 'fail';
+            }
+        }
+    }
+}
+
+const seedDB = async () => {
+    User.findByIdAndUpdate(mainAuth, { $set: { campgrounds: [] } });
+    try {
+        await Campground.deleteMany({});
+
+
+        const res = await processDatas();
+        res.data.data.forEach(async (camp) => {
+            if (camp.images[0]) {
+                const price = Math.floor(Math.random() * 20) + 10;
+                const campground = new Campground({
+                    author: mainAuth,
+                    //do reverse lookup here!!!!!from the coordinates if no address available
+                    location: camp.addresses[0] ?
+                        `${camp.addresses[0].line1} ${camp.addresses[0].city} ${camp.addresses[0].stateCode}` :
+                        await reverseGeo([Number.parseFloat(camp.longitude, 10), Number.parseFloat(camp.latitude, 10)]),
+
+                    title: camp.name,
+
+                    description: camp.description,
+                    //assign a random price if there is no cost
+                    price: camp.fees[0] ? camp.fees[0].cost : price,
+
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [
+                            camp.longitude,
+                            camp.latitude
+                        ]
+                    }
+                })
+                await upload(camp.images.map(img => img.url), campground);
+                await User.findByIdAndUpdate(mainAuth, { $push: { campgrounds: campground } });
+                if (campground.success !== 'fail') {
+                    await campground.save();
+                }
+                // await campground.save();
+
+            }
+        })
+
+    } catch (error) {
+        console.log("TIMEOUT:", error)
+    }
+}
+
+seedDB()
+    .then(() => {
+        console.log('done')
+    })
+    .catch(e => {
+        console.log("ERROR SEEDING", e)
+    })
